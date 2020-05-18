3af3a73367c6146ca77b42f0f78b9a541300c062
John Pape <jtpape@us.ibm.com>
2020-05-18 16:45:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-data "ibm_is_vpc" "vpc1" {
-  name = "us-east-v2"
-}
-
 resource "ibm_is_subnet" "subnet1" {
   name            = "got-relationships-subnet"
-  vpc             = ibm_is_vpc.vpc1.id
+  vpc             = var.vpc
   zone            = var.zone1
   ipv4_cidr_block = "10.240.0.0/28"
 }
@@ -19,7 +15,7 @@ resource "ibm_is_instance" "instance1" {
     subnet = ibm_is_subnet.subnet1.id
   }
 
-  vpc       = ibm_is_vpc.vpc1.id
+  vpc       = var.vpc
   zone      = var.zone1
   keys      = ["r014-13838d07-5ae1-42c5-bc70-d284dbfcdfbb"]
   user_data = file("./cloud-init.txt")
