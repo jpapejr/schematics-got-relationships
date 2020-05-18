6b13599951a7f5255ae633f04b8fbd864fe8b02e
John Pape <jtpape@us.ibm.com>
2020-05-18 17:13:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
+resource "ibm_is_public_gateway" "publicgateway1" {
+  name = "gateway1"
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone1
+}
+
 resource "ibm_is_subnet" "subnet1" {
   name            = "got-relationships-subnet"
   vpc             = var.vpc
   zone            = var.zone1
   ipv4_cidr_block = "10.240.0.0/28"
+  public_gateway  = ibm_is_public_gateway.publicgateway1.id
 }
 
 
